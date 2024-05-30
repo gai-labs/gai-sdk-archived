7f5257392f2e2a6d29253cf80e6fcd891484a4ef
RoyLai-InfoCorp <roy.lai@infocorp.io>
2024-05-30 14:18:47 +0800

    Updated dockerfile to fix working directory issue

--- gai-gen/tests/integration_tests/print_delta.py
+++ gai-gen/tests/integration_tests/print_delta.py
@@ -3,6 +3,9 @@ import json
 
 for line in sys.stdin:
     result = json.loads(line)
-    content = result["choices"][0]["delta"]["content"]
-    if content:
-        print(content, end="", flush=True)
+    try:
+        content = result["choices"][0]["delta"]["content"]
+        if content:
+            print(content, end="", flush=True)
+    except Exception as e:
+        print("Error: ", e,". Cannot parse ", result, file=sys.stderr, flush=True)
